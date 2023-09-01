dd7f3d729e715a9dff86ecb2ea70329a43973ffb
mgalihpp <muhammadgalih451@gmail.com>
2023-09-01 23:39:42 +0700

    add cookies to auto login

--- src/store/index.js
+++ src/store/index.js
@@ -1,20 +1,22 @@
 import { configureStore, createSlice } from "@reduxjs/toolkit";
+import Cookies from "js-cookie";
 
 const authSlice = createSlice({
   name: "auth",
-  initialState: { isLogged: false },
+  initialState: { isLogged: Cookies.get("userId") },
   reducers: {
     login(state) {
       state.isLogged = true;
     },
     logout(state) {
       state.isLogged = false;
+      Cookies.remove("userId")
     },
   },
 });
 
-export const authAction = authSlice.actions
+export const authAction = authSlice.actions;
 
 export const store = configureStore({
-    reducer: authSlice.reducer
-})
+  reducer: authSlice.reducer,
+});
