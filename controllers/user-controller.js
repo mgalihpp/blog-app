55857093905bde8f75f8cc0cee9890cd06a99049
mgalihpp <muhammadgalih451@gmail.com>
2023-09-01 23:00:07 +0700

    sss

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,63 +1,79 @@
 import UserModel from "../models/User.js";
-import bcrypt from 'bcryptjs'
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
-export const getAllUser = async(req, res, next) => {
-    let users;
-    try {
-        users = await UserModel.find()
-    } catch (error) {
-        console.error(error);
-    }
-    if(!users){
-        return res.status(404).json({message: "no users found"});
-    }
-    return res.status(200).json({users});
-}
+const SECRET_KEY = "MAOGMOAMGGMO2PLPAAA2";
+
+export const getAllUser = async (req, res, next) => {
+  let users;
+  try {
+    users = await UserModel.find();
+  } catch (error) {
+    console.error(error);
+  }
+  if (!users) {
+    return res.status(404).json({ message: "no users found" });
+  }
+  return res.status(200).json({ users });
+};
 
 export const signup = async (req, res, next) => {
-    const {name, email, password} = req.body;
-    
-    let existingUser;
+  const { name, email, password } = req.body;
 
-    try {
-        existingUser = await UserModel.findOne({email});
-    } catch (error) {
-        console.error(error);
-    }
-    if(existingUser){
-        return res.status(400).json({message : "User Already Exits! Login instead"})
-    }
-    const hashPassword = bcrypt.hashSync(password);
-    const newUser = new UserModel({
-        name,
-        email,
-        password: hashPassword,
-        blogs: []
-    });
+  let existingUser;
 
-    try {
-        newUser.save();
-    } catch (error) {
-        console.error(error);
-    }
-    return res.status(201).json({newUser})
-}
+  try {
+    existingUser = await UserModel.findOne({ email });
+  } catch (error) {
+    console.error(error);
+  }
+  if (existingUser) {
+    return res
+      .status(400)
+      .json({ message: "User Already Exits! Login instead" });
+  }
+  const hashPassword = bcrypt.hashSync(password);
+  const newUser = new UserModel({
+    name,
+    email,
+    password: hashPassword,
+    avatar,
+    blogs: [],
+  });
 
-export const login = async (req, res, next) =>{
-    const { email, password } = req.body;
-    let existingUser;
-    try {
-        existingUser = await UserModel.findOne({email});
-    } catch (error) {
-        console.error(error);
-    }
-    if(!existingUser){
-        return res.status(404).json({message : "Couldnt find user by this email, please register"})
-    }
+  try {
+    newUser.save();
+  } catch (error) {
+    console.error(error);
+  }
+  return res.status(201).json({ newUser });
+};
 
-    const isPasswordCorrect = bcrypt.compareSync(password, existingUser.password);
-    if(!isPasswordCorrect){
-        return res.status(404).json({ message : "Incorrect Password"});
-    }
-    return res.status(200).json({ message: "Login Successfull", users: existingUser})
-}
+export const login = async (req, res, next) => {
+  const { email, password } = req.body;
+  let existingUser;
+  try {
+    existingUser = await UserModel.findOne({ email });
+  } catch (error) {
+    console.error(error);
+  }
+  if (!existingUser) {
+    return res
+      .status(404)
+      .json({ message: "Couldnt find user by this email, please register" });
+  }
+
+  const isPasswordCorrect = bcrypt.compareSync(password, existingUser.password);
+  if (!isPasswordCorrect) {
+    return res.status(404).json({ message: "Incorrect Password" });
+  }
+
+  const authToken = jwt.sign({ userId: existingUser._id}, SECRET_KEY);
+  res.cookie('authToken', authToken, {
+    httpOnly: true,
+  });
+
+  return res
+    .status(200)
+    .json({ message: "Login Successfull", users: existingUser });
+};
--- models/User.js
+++ models/User.js
@@ -15,6 +15,10 @@ const UserSchema = new mongoose.Schema({
     required: true,
     minlength: 6,
   },
+  avatar: {
+    type: String,
+    default: "https://i.pinimg.com/736x/1b/b8/c8/1bb8c82cdad7277dd52ae4d21c7ee292.jpg",
+  },
   blogs: [{ type: mongoose.Types.ObjectId, ref: "blogs", required: true }],
 });
 
