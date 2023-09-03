b99230fd5897f1d59823a8f97abbeb38e59990f0
mgalihpp <muhammadgalih451@gmail.com>
2023-09-03 13:18:23 +0700

    add user setting

--- src/App.js
+++ src/App.js
@@ -4,15 +4,15 @@ import { Route, Routes, Navigate } from "react-router-dom";
 import Login from "./components/Login";
 import Blog from "./components/Blog";
 import UserBlog from "./components/UserBlog";
-import BlogDetail from "./components/BlogDetail";
 import AddBlog from "./components/AddBlog";
 import { darkTheme, lightTheme } from "./Theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import Cookies from "js-cookie";
+import UserSetting from "./pages/UserSetting";
 
 function App() {
   const [isDarkMode, setIsDarkMode] = useState(
-    Cookies.get('theme') === 'dark' ? true : false
+    Cookies.get("theme") === "dark" ? true : false
   );
 
   const toggleTheme = () => {
@@ -36,8 +36,9 @@ function App() {
             <Route path="/auth" element={<Login />} />
             <Route path="/blogs" element={<Blog />} />
             <Route path="/myblogs" element={<UserBlog />} />
-            <Route path="/myblogs/:id" element={<BlogDetail />} />
+            <Route path="/myblogs/:id" element={<UserBlog />} />
             <Route path="/blogs/add" element={<AddBlog />} />
+            <Route path="/user/settings" element={<UserSetting />} />
           </Routes>
         </main>
       </React.Fragment>
