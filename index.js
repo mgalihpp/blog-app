90e6ead157786463a7cbceb76ae56bcb64049c33
mgalihpp <muhammadgalih451@gmail.com>
2023-09-03 13:17:13 +0700

    add compression

--- index.js
+++ index.js
@@ -1,34 +1,33 @@
-import express from 'express';
-import cors from 'cors';
-import 'dotenv/config'
-import mongoose from 'mongoose';
-import userRouter from './routes/user-routes.js';
-import blogRouter from './routes/blog-routes.js';
-import cookieParser from 'cookie-parser';
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import mongoose from "mongoose";
+import userRouter from "./routes/user-routes.js";
+import blogRouter from "./routes/blog-routes.js";
+import cookieParser from "cookie-parser";
+import compression from "compression";
 
 const PORT = process.env.PORT;
 const URL = process.env.MONGOOSE_URL;
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-    origin: 'https://mgalihpp.github.io',
-    credentials: true,
-  }));
+app.use(cors());
+app.use(compression())
 
 app.use("/api/user", userRouter);
 app.use("/api/blog", blogRouter);
 app.get("/", async (req, res) => {
-    res.send('ok')
-})
+  res.send("ok");
+});
 
 try {
-    mongoose.connect(URL)
-    console.log("connected to mongodb");
+  mongoose.connect(URL);
+  console.log("connected to mongodb");
 } catch (error) {
-    console.error(error);
+  console.error(error);
 }
 
 app.listen(PORT, () => {
-    console.log(PORT, 'listening...');
-})
+  console.log(PORT, "listening...");
+});
